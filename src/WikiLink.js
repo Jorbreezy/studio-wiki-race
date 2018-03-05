f177375928d6cb59536958b07b061376f7fa9cff
Jordan Kelly <jkelly2@aoiths.org>
2018-03-05 23:28:13 +0000

    Hello

---
+++ src/WikiLink.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import WikiApi from './WikiApi.js';
+
+export default class WikiLink extends Component {
+  //Called auto, component is ready to be displayed
+  
+  constructor(props){
+      super(props);
+      this.state = {
+          summary: "Loading..."
+      };
+  }
+  
+  componentDidMount(){
+      
+      var self = this;
+      
+    WikiApi.getSummary(this.props.title).then(function(text){
+       self.setState({
+        summary: text   
+       }); 
+    });
+  }
+  
+  render() {
+    var currentTitle = this.props.title;
+    
+    return (
+      <div>
+        {this.state.summary} 
+      </div>
+    );
+  }
+}
