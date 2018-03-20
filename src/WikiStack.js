c167cb6a05f1ce65094ab35db88023696ea26415
Jordan Kelly <jkelly2@aoiths.org>
2018-03-20 02:14:55 +0000

    Hello

--- src/WikiStack.js
+++ src/WikiStack.js
@@ -12,9 +12,13 @@ export default class WikiStack extends Component {
     var currentTitle = this.props.title;
     // { this.state.links.map(n => titles={this.props.titles(n)} {n}) }
     
+        // {this.state.links.map(n => <li key={n} style={{ listStyle: 'none' }} onClick={() => this.props.addTitle(n) }> {n} <hr /></li>) }
+        // { this.props.titles.map(n => <button onClick={this.props.setCurrentTitle(n)}> {n} </button>) }
+    
+    
     return (
       <div>
-        { this.props.titles.map(n => <button onClick={this.props.setCurrentTitle(n)}> {n} </button>) }
+        { this.props.titles.map( n => <button key={n} onClick={() => this.props.setCurrentTitle(n) }> {n} </button>) } 
       </div>
     );
   }
